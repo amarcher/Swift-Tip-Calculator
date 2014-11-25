f233d6dd547dfaa0d9dac63390a459c8af4480d3
Andrew Archer <andrew.m.archer@gmail.com>
2014-11-24 22:45:53 -0800

    initial version

--- CalculateDatTip/ViewController.swift
+++ CalculateDatTip/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var fifteenPercentLabel: UILabel!
+    @IBOutlet weak var eighteenPercentLabel: UILabel!
+    @IBOutlet weak var twentyPercentLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func calculateTipAmountFor(tipPct: Float) -> NSString {
+        let tipMultiplier: Float = tipPct/100
+        let totalAmount = (amountTextField.text as NSString).floatValue
+        let calculatedTipAmount = tipMultiplier * totalAmount
+        let totalString = NSString(format:"$%.2f",calculatedTipAmount)
+        return totalString
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        fifteenPercentLabel.text = calculateTipAmountFor(15)
+        eighteenPercentLabel.text = calculateTipAmountFor(18)
+        twentyPercentLabel.text = calculateTipAmountFor(20)
+    }
 }
 
